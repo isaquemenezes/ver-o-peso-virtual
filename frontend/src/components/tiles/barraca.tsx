ea0305ae67e3d09017b56675e59f43b225d57c9e
Enzo Santos <enzogabrielrocha29@gmail.com>
2022-08-05 07:48:50 -0300

    Atualiza cartão de barraca

--- frontend/src/components/tiles/barraca.tsx
+++ frontend/src/components/tiles/barraca.tsx
@@ -1,5 +1,6 @@
 import {Barraca, TipoBarraca} from "../../lib/modelos";
 import React, {Component} from "react";
+import {Typography, Card, CardContent, CardActions, Button} from '@mui/material';
 
 type _Props = { barraca: Barraca };
 type _State = {};
@@ -7,21 +8,31 @@ type _State = {};
 export default class CartaoBarraca extends Component<_Props, _State> {
   render() {
     const barraca: Barraca = this.props.barraca;
-    return <div>
-      <p>{barraca.nome}</p>
-      <p>{(() => {
-        const labels = barraca.tipos.map((tipo) => {
-          switch (tipo) {
-            case TipoBarraca.artesanatos:
-              return 'artesanatos';
-            case TipoBarraca.comidas:
-              return 'comidas';
-            case TipoBarraca.roupas:
-              return 'roupas';
-          }
-        });
-        return <p>Inclui {labels.join(', ')}</p>;
-      })()}</p>
-    </div>;
+    return <Card sx={{ minWidth: 275 }}>
+      <CardContent style={{textAlign: "left"}}>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Barraca
+        </Typography>
+        <Typography variant="h5" component="div">
+          {barraca.nome}
+        </Typography>
+        <Typography variant="body2">{(() => {
+          const labels = barraca.tipos.map((tipo) => {
+            switch (tipo) {
+              case TipoBarraca.artesanatos:
+                return 'artesanatos';
+              case TipoBarraca.comidas:
+                return 'comidas';
+              case TipoBarraca.roupas:
+                return 'roupas';
+            }
+          });
+          return <p>Inclui {labels.join(', ')}</p>;
+        })()}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Ver barraca</Button>
+      </CardActions>
+    </Card>;
   }
 }

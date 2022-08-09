3b3054668741f96be8814c5d5d5d62494b42b568
Isaque Menezes <isaque-menezes@hotmail.com>
2022-08-09 17:44:55 -0300

    feature cadastro de usuario sem validacoes e tratamento

--- app.js
+++ app.js
@@ -44,6 +44,21 @@ app.get('/login', function (req, res) {
 app.get('/register', function (req, res) {
     res.render('register');
 });
+app.post("/controllerRegister", urlEncodeParser, function (req, res){
+    sql.query( "INSERT INTO usuario values (?,?,?,?,?,?,?)",
+                [
+                    req.body.id,
+                    req.body.nome,
+                    req.body.fone,            
+                    req.body.email,
+                    req.body.password,
+                    req.body.user,
+                    req.body.dateCreate
+                ]
+            );
+    res.render('controllerRegister');
+
+});
 
 // Criar Um Novo Registro
 app.get('/inserir', function (req, res) {
